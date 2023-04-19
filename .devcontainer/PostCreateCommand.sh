a9ff3a7bf07baec0d965c2e22efca71337528291
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2023-04-19 11:38:53 +0000

    Configuring Api in github codspace

---
+++ .devcontainer/PostCreateCommand.sh
@@ -0,0 +1,4 @@
+dotnet tool install --global dotnet-ef --version 7.0.5
+dotnet restore aspnet-core/WriteFluency/WriteFluency.csproj
+dotnet ef database update --project aspnet-core/WriteFluency/WriteFluency.csproj
+dotnet dev-certs https
