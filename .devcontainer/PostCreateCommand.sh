0921e874d9fc8eb00d86efee034a71e74745eb4e
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2023-04-20 10:54:28 +0000

    fixing PostCreateCommand npm install

--- .devcontainer/PostCreateCommand.sh
+++ .devcontainer/PostCreateCommand.sh
@@ -2,5 +2,5 @@ dotnet tool install --global dotnet-ef --version 7.0.5
 dotnet restore aspnet-core/WriteFluency/WriteFluency.csproj
 dotnet ef database update --project aspnet-core/WriteFluency/WriteFluency.csproj
 dotnet dev-certs https
-cd angular
+cd WriteFluencyApp
 npm install
