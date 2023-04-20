04e311ff07055c392b38ba0b48598221a6df308f
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2023-04-20 00:07:22 +0000

    Configuring code space for angular

--- .devcontainer/PostCreateCommand.sh
+++ .devcontainer/PostCreateCommand.sh
@@ -2,3 +2,5 @@ dotnet tool install --global dotnet-ef --version 7.0.5
 dotnet restore aspnet-core/WriteFluency/WriteFluency.csproj
 dotnet ef database update --project aspnet-core/WriteFluency/WriteFluency.csproj
 dotnet dev-certs https
+cd angular
+npm install
