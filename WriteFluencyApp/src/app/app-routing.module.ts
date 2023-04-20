e63e951d454238371a623f417838e578a616bdb7
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2023-04-20 10:57:01 +0000

    adding listen and write component

--- WriteFluencyApp/src/app/app-routing.module.ts
+++ WriteFluencyApp/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ListenAndWriteComponent } from './listen-and-write/listen-and-write.component';
 
 const routes: Routes = [
   // load home component when the path is empty
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'listen-and-write', component: ListenAndWriteComponent },
 ];
 
 @NgModule({
