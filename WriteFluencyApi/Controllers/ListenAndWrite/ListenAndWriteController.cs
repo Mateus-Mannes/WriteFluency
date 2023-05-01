023681709f08a6a8cb6f642440a16a91c054f936
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2023-05-01 19:30:49 +0000

    starting access to open ai api

---
+++ WriteFluencyApi/Controllers/ListenAndWrite/ListenAndWriteController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WriteFluencyApi.Controllers.ListenAndWrite;
+
+[ApiController]
+[Route("[controller]")]
+public class ListenAndWriteController : ControllerBase
+{
+    
+}
---
+++ WriteFluencyApi/Shared/ListenAndWrite/SubjectEnum.cs
@@ -0,0 +1,13 @@
+namespace WriteFluencyApi.Shared.ListenAndWrite;
+
+public enum SubjectEnum
+{
+    Travel = 1,
+    Culture = 2,
+    Business = 3,
+    Finance = 4,
+    Work = 5,
+    TechnologyAndInnovation = 6,
+    Entertainment = 7,
+    Science = 8
+}
