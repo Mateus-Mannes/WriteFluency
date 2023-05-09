0e47962288b600c160a79f82f6be89d3507e5e46
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2023-05-09 12:52:53 +0000

    Making speech to text api request

--- WriteFluencyApi/ExternalApis/TextToSpeech/TextToSpeechApi.cs
+++ WriteFluencyApi/ExternalApis/TextToSpeech/TextToSpeechApi.cs
@@ -1,6 +1,44 @@
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Options;
+using WriteFluencyApi.ExternalApis.TextToSpeech.Requests;
+using WriteFluencyApi.ExternalApis.TextToSpeech.Responses;
+using WriteFluencyApi.Services.ListenAndWrite;
+
 namespace WriteFluencyApi.ExternalApis.TextToSpeech;
 
-public class TextToSpeechApi
+public class TextToSpeechApi : ISpeechGenerator
 {
-    
+    private readonly HttpClient _httpClient = new HttpClient();
+    private readonly TextToSpeechConfig _textToSpeechConfig;
+
+    public TextToSpeechApi(IOptions<TextToSpeechConfig> textToSpeechConfig)
+    {
+        _textToSpeechConfig = textToSpeechConfig.Value;
+        _httpClient.BaseAddress = new Uri(_textToSpeechConfig.BaseAddress);
+        _httpClient.DefaultRequestHeaders.Accept.Clear();
+        _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        _httpClient.DefaultRequestHeaders.Add(_textToSpeechConfig.KeyName, _textToSpeechConfig.Key);
+    }
+
+    public async Task<byte[]> GenerateSpeechAsync(string text)
+    {
+        var request = new TextToSpeechRequest(
+            new Input(text),
+            new AudioConfig("OGG_OPUS"),
+            new Voice("en-US")
+        );
+
+        var response = await _httpClient.PostAsJsonAsync(_textToSpeechConfig.Routes.TextSynthesize, request);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadFromJsonAsync<TextToSpeechResponse>()
+                ?? throw new HttpRequestException("Error fetching data from TextToSpeech API");
+            return Convert.FromBase64String(result.AudioContent);
+        }
+        else
+        {
+            throw new HttpRequestException($"Error fetching data from TextToSpeech API: {response.StatusCode}");
+        }
+    }
 }
--- WriteFluencyApi/Program.cs
+++ WriteFluencyApi/Program.cs
@@ -1,4 +1,5 @@
 using WriteFluencyApi.ExternalApis.OpenAI;
+using WriteFluencyApi.ExternalApis.TextToSpeech;
 using WriteFluencyApi.Services.ListenAndWrite;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddScoped<ITextGenerator, OpenAIApi>();
+builder.Services.AddScoped<ISpeechGenerator, TextToSpeechApi>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -14,12 +16,13 @@ builder.Services.AddSwaggerGen();
 // Add configs
 builder.Configuration.AddEnvironmentVariables();
 builder.Services.Configure<OpenAIConfig>(builder.Configuration.GetSection(OpenAIConfig.Config));
+builder.Services.Configure<TextToSpeechConfig>(builder.Configuration.GetSection(TextToSpeechConfig.Config));
 
 var app = builder.Build();
 
-var clients = app.Configuration.GetValue<string>("AlloewdClients").Split(',');
+var clients = app.Configuration.GetValue<string>("AlloewdClients")?.Split(',');
 app.UseCors(x => x
-    .WithOrigins(clients)
+    .WithOrigins(clients ?? new string[0])
     .AllowAnyMethod()
     .AllowAnyHeader());
 
