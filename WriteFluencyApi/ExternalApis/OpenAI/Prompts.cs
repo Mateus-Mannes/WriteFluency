21140504e83a068144d788c29824127e02af7493
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2023-05-10 10:43:57 +0000

    adding Description to SubjectEnum

--- WriteFluencyApi/ExternalApis/OpenAI/Prompts.cs
+++ WriteFluencyApi/ExternalApis/OpenAI/Prompts.cs
@@ -7,7 +7,7 @@ public static class Prompts
 {
     public static string GenerateText(GeneratePropositionDto dto) 
         => @$"
-            Write about some subject related to {dto.Subject}.
+            Write about some subject related to {dto.Subject.GetDescription()}.
             Maximum of one paragraph, from 250 to 600 characteres.
             Write it in a way that normal people can understand well, without specialist vocabulary.
             Write just the text please, without title.
