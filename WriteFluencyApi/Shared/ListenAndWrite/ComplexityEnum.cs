bf6ea37ce05c54c626ff8c77cc74429ee42e4342
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2023-05-10 00:12:22 +0000

    Adicionando JsonConverter nos enums

--- WriteFluencyApi/Shared/ListenAndWrite/ComplexityEnum.cs
+++ WriteFluencyApi/Shared/ListenAndWrite/ComplexityEnum.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel;
+using System.Text.Json.Serialization;
 
 namespace WriteFluencyApi.Shared.ListenAndWrite;
 
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum ComplexityEnum
 {
     [Description("Be simple and beginner friendly, using easy vocabulary and simple sentence structures.")]
