3bf58c265a6e895a3bbbde1b73ff1e6519c2638c
Mateus Mannes De Medeiros <mateus-mannes@hotmail.com>
2023-05-17 00:41:00 +0000

    Adicionando progress bar para gerar o audio

--- WriteFluencyApp/src/app/listen-and-write/component/listen-and-write.component.ts
+++ WriteFluencyApp/src/app/listen-and-write/component/listen-and-write.component.ts
@@ -1,9 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/enviroments/enviroment';
 import { Topics } from '../entities/topics';
 import { DropDownComponent } from 'src/app/shared/drop-down/drop-down.component';
 import { Proposition } from '../entities/proposition';
+import { take, timer } from 'rxjs';
 
 @Component({
   selector: 'app-listen-and-write',
@@ -14,12 +15,15 @@ export class ListenAndWriteComponent implements OnInit {
 
   constructor(
     private readonly _httpClient: HttpClient,
+    private readonly renderer: Renderer2
   ) {}
 
   private readonly route = `${environment.apiUrl}/listen-and-write`;
   @ViewChild('complexity') complexity!: DropDownComponent;
   @ViewChild('subject') subject!: DropDownComponent;
   @ViewChild('audioPlayer') audioPlayer!: ElementRef;
+  @ViewChild('progress') progress!: ElementRef;
+  @ViewChild('progressBar') progressBar!: ElementRef;
   complexities: string[] = [];
   subjects: string[] = [];
   
@@ -34,9 +38,27 @@ export class ListenAndWriteComponent implements OnInit {
   generateAudio(){
     let complexity = this.complexity.selectedOption;
     let subject = this.subject.selectedOption;
+
+    // Start the progress bar animation
+    this.renderer.setStyle(this.progress.nativeElement, 'width', `0%`);
+    this.progressBar.nativeElement.hidden = false;
+    this.audioPlayer.nativeElement.hidden = true;
+    const duration = 10; // duration in seconds
+    const interval = 100; // update interval in milliseconds
+    const steps = duration * 1000 / interval;
+    const increment = 100 / steps;
+    let width = 0;
+
+    timer(0, interval).pipe(take(steps)).subscribe(() => {
+      width += increment;
+      this.renderer.setStyle(this.progress.nativeElement, 'width', `${width}%`);
+    });
+
     this._httpClient.post<Proposition>(`${this.route}/generate-proposition`, {complexity, subject})
       .subscribe(result => {
         this.audioPlayer.nativeElement.src = 'data:audio/ogg;base64,' + result.audio;
+        this.progressBar.nativeElement.hidden = true;
+        this.audioPlayer.nativeElement.hidden = false;
       });
   }
 }
