f3758fe823685508c1b2edc129b3dd91fe70f512
Mateus Mannes De Medeiros <mateus.medeiros@becomex.com.br>
2023-05-31 11:51:11 -0300

    adding descriptions

--- WriteFluencyApp/src/app/listen-and-write/listen-and-write.component.ts
+++ WriteFluencyApp/src/app/listen-and-write/listen-and-write.component.ts
@@ -2,6 +2,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { VerificationComponent } from './verification/verification.component';
 import { PropositionComponent } from './proposition/proposition.component';
+import { AlertService } from '../shared/services/alert-service';
 
 @Component({
   selector: 'app-listen-and-write',
@@ -11,13 +12,18 @@ import { PropositionComponent } from './proposition/proposition.component';
 export class ListenAndWriteComponent {
 
   constructor(
-    private readonly _dialog: MatDialog)
+    private readonly _dialog: MatDialog,
+    private readonly _alertSrvice: AlertService)
   { }
 
   @ViewChild('textarea') textarea!: ElementRef;
   @ViewChild('proposition') proposition!: PropositionComponent;
 
   textAreaContent = '';
+
+  ngAfterViewInit() {
+    this._alertSrvice.alert('Select a complexity level and a subject. Gerenate an audio proposition and try to write it ! Then verify your text.', 'info', 20000);
+  }
 
   verify() {
     const dialog = this._dialog.open(VerificationComponent, 
