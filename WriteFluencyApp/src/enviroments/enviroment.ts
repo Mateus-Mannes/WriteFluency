3070ed185c8ef09477984e94886b3835ab5f617f
Mateus Mannes De Medeiros <mateus.medeiros@becomex.com.br>
2023-05-31 09:33:36 -0300

    finishing diving in components

--- WriteFluencyApp/src/enviroments/enviroment.ts
+++ WriteFluencyApp/src/enviroments/enviroment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: "https://mateus-mannes-cautious-succotash-4679qwr6v4jfjvw4-44317.preview.app.github.dev"
+    apiUrl: "https://localhost:44317"
 };
