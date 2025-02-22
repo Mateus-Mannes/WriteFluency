7ee26548186a0245c72711638cd22583d0a9305c
Mateus Medeiros <mateus.medeiros@radixeng.com>
2025-02-22 14:35:34 -0300

    Adding google Oauth authentication, including swagger configuration

--- WriteFluencyApi/src/Program.cs
+++ WriteFluencyApi/src/Program.cs
@@ -1,10 +1,6 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using WriteFluencyApi.Configuration;
 using WriteFluencyApi.Data;
 using WriteFluencyApi.Domain.Login;
 using WriteFluencyApi.ExternalApis.OpenAI;
@@ -13,44 +9,27 @@ using WriteFluencyApi.ListenAndWrite.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-// Add configs
+builder.Services.AddAppSwagger();
+
+builder.AddAppAuthentication();
+
+// Options configuration
 builder.Configuration.AddEnvironmentVariables();
 builder.Services.Configure<OpenAIOptions>(builder.Configuration.GetSection(OpenAIOptions.Section));
 builder.Services.Configure<TextToSpeechOptions>(builder.Configuration.GetSection(TextToSpeechOptions.Section));
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(JwtOptions.Section));
 
-// add services
-
+// Adds the database context and identity configuration
 builder.Services.AddDbContext<ApiDbContext>(opts => opts.UseSqlite("Data Source=data.db"));
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(opts =>
-    {
-        opts.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
-        };
-    });
-
 builder.Services.AddIdentityCore<IdentityUser>()
     .AddApiEndpoints()
     .AddEntityFrameworkStores<ApiDbContext>();
 
-builder.Services.AddAuthorization();
-
+// Adding domain services
 builder.Services.AddHttpClient();
 builder.Services.AddTransient<ITextGenerator, OpenAIApi>();
 builder.Services.AddTransient<ISpeechGenerator, TextToSpeechApi>();
@@ -67,20 +46,24 @@ builder.Services.AddCors();
 
 var app = builder.Build();
 
-var clients = app.Configuration.GetValue<string>("AlloewdClients")?.Split(',');
+var clients = app.Configuration.GetValue<string>("AllowedClients")?.Split(',');
 app.UseCors(opts =>
 {
     if (clients![0] != "*") opts.WithOrigins(clients);
     else opts.AllowAnyOrigin();
-    opts.AllowAnyMethod();
-    opts.AllowAnyHeader();
 });
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => {
+        // Configures the swagger ui to use the google login
+        c.OAuthClientId(builder.Configuration["Authentication:Google:ClientId"]);
+        c.OAuthClientSecret(builder.Configuration["Authentication:Google:ClientSecret"]);
+        c.OAuthUsePkce(); 
+        c.OAuthScopes("openid", "profile", "email");
+    });
 }
 
 app.UseHttpsRedirection();
