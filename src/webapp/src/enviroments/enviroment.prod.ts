bf545c994f83631060a10313efc91b03c0a4f3b3
Mateus Medeiros <mateus.medeiros@radixeng.com>
2025-07-21 09:38:14 -0300

    Adding ingress to http services

--- src/webapp/src/enviroments/enviroment.prod.ts
+++ src/webapp/src/enviroments/enviroment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: "http://writefluency.com:8080",
+    apiUrl: "http://writefluency.com/api",
     otlpEndpoint: "",
     instrumentationKey: "768f1e67-fda1-4093-8e79-227c26b50895"
 };
