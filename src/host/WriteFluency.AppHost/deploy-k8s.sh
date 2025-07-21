f1cec1047f5b243d16f7bf21dba8f2e7ecece389
Mateus Medeiros <mateus.medeiros@radixeng.com>
2025-07-21 11:01:40 -0300

    Adding TLS to ingress

--- src/host/WriteFluency.AppHost/deploy-k8s.sh
+++ src/host/WriteFluency.AppHost/deploy-k8s.sh
@@ -6,4 +6,6 @@ KUBE_CONTEXT=$1
 
 ./generate-k8s-secret.sh
 
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/latest/download/cert-manager.yaml
+
 aspirate apply -i ./aspirate-overlays --non-interactive --kube-context "$KUBE_CONTEXT"
--- src/webapp/src/enviroments/enviroment.prod.ts
+++ src/webapp/src/enviroments/enviroment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: "http://writefluency.com/api",
+    apiUrl: "http://api.writefluency.com",
     otlpEndpoint: "",
     instrumentationKey: "768f1e67-fda1-4093-8e79-227c26b50895"
 };
