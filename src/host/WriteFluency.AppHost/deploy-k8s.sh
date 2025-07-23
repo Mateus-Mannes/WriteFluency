c72128184931caa4c4711de6828dc08d6a591cac
Mateus Medeiros <mateus.medeiros@radixeng.com>
2025-07-23 08:25:26 -0300

    Adding wating time for cert-manager

--- src/host/WriteFluency.AppHost/deploy-k8s.sh
+++ src/host/WriteFluency.AppHost/deploy-k8s.sh
@@ -8,4 +8,10 @@ KUBE_CONTEXT=$1
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/latest/download/cert-manager.yaml
 
+echo "⏳ Waiting for cert-manager to be ready..."
+kubectl rollout status deployment cert-manager -n cert-manager --timeout=120s
+kubectl rollout status deployment cert-manager-webhook -n cert-manager --timeout=120s
+kubectl rollout status deployment cert-manager-cainjector -n cert-manager --timeout=120s
+
+
 aspirate apply -i ./aspirate-overlays --non-interactive --kube-context "$KUBE_CONTEXT"
