16611f5f136dd6a2754f8544683af4d726d8f332
Mateus Medeiros <mateus.medeiros@radixeng.com>
2025-07-28 10:29:29 -0300

    Using https on the api url

--- src/webapp/src/enviroments/enviroment.prod.ts
+++ src/webapp/src/enviroments/enviroment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: "http://api.writefluency.com",
+    apiUrl: "https://api.writefluency.com",
     otlpEndpoint: "",
     instrumentationKey: "768f1e67-fda1-4093-8e79-227c26b50895"
 };
