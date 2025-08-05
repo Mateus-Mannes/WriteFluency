7ff58ca2cf3353cd9a6232dc7f8410ec095c65ef
Mateus Medeiros <mateus.medeiros@radixeng.com>
2025-08-05 08:31:27 -0300

    Fixing prod api url

--- src/webapp/src/enviroments/enviroment.prod.ts
+++ src/webapp/src/enviroments/enviroment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: "https://api.writefluency.com",
+    apiUrl: "https://api.writefluency.com/api",
     otlpEndpoint: "",
     instrumentationKey: "768f1e67-fda1-4093-8e79-227c26b50895"
 };
