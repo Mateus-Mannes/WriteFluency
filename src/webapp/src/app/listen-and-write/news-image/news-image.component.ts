ee6ec1a614861828fd580d4a311c283f3cfb794f
Mateus Medeiros <mateus.medeiros@radixeng.com>
2025-12-08 09:08:50 -0300

    Add news image component

--- src/webapp/src/app/listen-and-write/news-image/news-image.component.ts
+++ src/webapp/src/app/listen-and-write/news-image/news-image.component.ts
@@ -1,8 +1,9 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-news-image',
-  imports: [],
+  imports: [ NgOptimizedImage ],
   templateUrl: './news-image.component.html',
   styleUrl: './news-image.component.scss',
 })
