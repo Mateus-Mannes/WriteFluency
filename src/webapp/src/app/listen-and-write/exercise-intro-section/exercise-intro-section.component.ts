5d4f4574c6252510efdc37772c8948758c644fbf
Mateus Medeiros <mateus.medeiros@radixeng.com>
2025-12-12 12:12:18 -0300

    Add intro section component

---
+++ src/webapp/src/app/listen-and-write/exercise-intro-section/exercise-intro-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-exercise-intro-section',
+  imports: [MatButtonModule, MatIconModule],
+  templateUrl: './exercise-intro-section.component.html',
+  styleUrl: './exercise-intro-section.component.scss',
+})
+export class ExerciseIntroSectionComponent {
+
+}
--- src/webapp/src/app/listen-and-write/listen-and-write.component.ts
+++ src/webapp/src/app/listen-and-write/listen-and-write.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NewsInfoComponent } from './news-info/news-info.component';
 import { NewsImageComponent } from './news-image/news-image.component';
 import { NewsAudioComponent } from './news-audio/news-audio.component';
+import { ExerciseIntroSectionComponent } from './exercise-intro-section/exercise-intro-section.component';
 
 @Component({
   selector: 'app-listen-and-write',
-  imports: [ NewsInfoComponent, NewsImageComponent, NewsAudioComponent ],
+  imports: [ NewsInfoComponent, NewsImageComponent, NewsAudioComponent, ExerciseIntroSectionComponent ],
   templateUrl: './listen-and-write.component.html',
   styleUrl: './listen-and-write.component.scss',
 })
