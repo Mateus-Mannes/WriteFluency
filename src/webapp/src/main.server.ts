54cbcc00b5698988575525d8bcfd887ad7822764
Mateus Medeiros <mateus.medeiros@radixeng.com>
2026-02-10 22:15:09 -0300

    Fix prod config

--- src/webapp/src/main.server.ts
+++ src/webapp/src/main.server.ts
@@ -1,6 +1,12 @@
+import { enableProdMode } from '@angular/core';
 import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
+import { environment } from './enviroments/enviroment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 const bootstrap = (context: BootstrapContext) =>
     bootstrapApplication(AppComponent, config, context);
--- src/webapp/src/main.ts
+++ src/webapp/src/main.ts
@@ -1,7 +1,12 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { environment } from './enviroments/enviroment';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 if (!environment.production) {
   void import('./telemetry/instrument');
